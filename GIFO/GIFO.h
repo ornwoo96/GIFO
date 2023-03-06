34e21146d66d275ef7266f296a0cd851cc69a740
ornwoo96 <dongou705@naver.com>
2023-03-06 19:16:33 +0900

    Initial Commit

---
+++ GIFO/GIFO.h
@@ -0,0 +1,18 @@
+//
+//  GIFO.h
+//  GIFO
+//
+//  Created by 김동우 on 2023/03/06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GIFO.
+FOUNDATION_EXPORT double GIFOVersionNumber;
+
+//! Project version string for GIFO.
+FOUNDATION_EXPORT const unsigned char GIFOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GIFO/PublicHeader.h>
+
+
