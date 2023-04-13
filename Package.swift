ddafcb418af1c7ea31a1e84d1b5b76794484f502
ornwoo96 <dongou705@naver.com>
2023-04-13 16:45:45 +0900

    chore: package 파일 정리

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "GIFO",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(
+            name: "GIFO",
+            targets: ["GIFO"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "GIFO",
+            path: "GIFO/Sources")
+    ]
+)
