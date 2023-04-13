8949e2d6cef4491fcabdc9ac5699b8007b064ef5
ornwoo96 <dongou705@naver.com>
2023-04-13 15:16:09 +0900

    test 버전

--- GIFOPackage/Package.swift
+++ GIFOPackage/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GIFOPackage",
-            dependencies: ["GIFO"]),
+            dependencies: []),
         .testTarget(
             name: "GIFOPackageTests",
             dependencies: ["GIFOPackage"]),
